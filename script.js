8d50620f4b4d9a037ddf92e23e755d7e9bb31845
Divya Vernekar <divyavernekar99@gmail.com>
2024-11-28 16:43:45 +0530

    first

---
+++ script.js
@@ -0,0 +1,108 @@
+// function cart(){
+//     alert("You Clicked cart");
+// }
+
+
+
+// const profile = {
+//     FullName : "Divya",
+//     follow : true,
+//     followers:156,
+//     posts:89,
+// };
+// console.log(profile);
+// let age =16;
+
+// age >= 18 ? console.log("yes") : console.log("no");
+
+// function check(){
+//     let number = prompt("Enter a number");
+//     if(number%2==0){
+//         console.log("even");
+//     }
+//     else{
+//         console.log("odd");
+//     }
+    
+    
+// }
+// let sum = 0;
+// for(let i=1 ; i<=100 ; i++){
+//     if(i%2==0){
+//         // sum = sum + i;
+//         console.log(i);
+//     }
+    
+
+// }
+
+// let game = 7;
+// let usernum = prompt("Guess the number");
+// while(usernum != game){
+//     usernum = prompt("Guessed number is wrong so guess again ");
+// }
+// console.log("Congratulations you entered right one");
+
+
+// let str = "Divya";
+// let size = 0;
+// for(let i of str){
+//     console.log("i=",i);
+//     size++;
+// }
+// console.log("string size = ",size);
+
+// str.length
+// let str = "DivyaVernekar"
+
+// template literals
+
+// let specialString = `This is a template literals`;
+// console.log(specialString);
+
+// why do we use template literals example;
+
+// let obj = {
+//     item : " pen",
+//     price : 10,
+// };
+// let output = `the cost of${obj.item} is ${obj.price} rupees`;
+// console.log(output);
+
+// previous method old
+// console.log("the cost of", obj.item , "is", obj.price, "rupees");
+
+// what is string interpolation
+// A way to embed expressions in string
+// expression like 1+2+3
+
+// let specialString = `This is a template literals ${
+//     5*4
+// }`;
+// console.log(specialString);
+
+// let logo = document.getElementsByClassName("nav-address");
+// // console.dir(logo);
+// console.log(logo);
+
+// let input = document.getElementsByTagName("i");
+// console.dir(input);
+
+// let d = document.querySelector("div");
+// console.dir(d.innerText);
+// let d = document.querySelector("div");
+// console.dir(d.innerHTML);
+
+let d = document.querySelector("div");
+d.style.backgroundColor = "Green";
+d.style.color = "red";
+d.style.fontSize = "1x";
+d.style.fontWeight = "bolder";
+d.style.borderBlockColor = "orange";
+
+
+
+
+
+
+
